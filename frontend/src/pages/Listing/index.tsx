7e89311f6b8194a70fcc8fad6a8df942d0c82d1e
Samuel Rodrigues <samuel.augusto2626@gmail.com>
2022-05-13 11:33:23 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,49 +7,59 @@ import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    /* React Hook useState (Manter o estado)
-    const[pagenumber, setPageNumber] = useState(0);
-    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+
+    //React Hook useState (Manter o estado)
+    /*const[pagenumber, setPageNumber] = useState(0);
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response =>{
             const data = response.data as MoviePage;
             setPageNumber(data.number);
         });
         */
-        const[pagenumber, setPageNumber] = useState(0);
-
-        useEffect(() =>{
-            axios.get(`${BASE_URL}/movies?size=12&page=1`)
-            .then(response =>{
+    const [pagenumber, setPageNumber] = useState(0);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pagenumber}&sort=id`)
+            .then(response => {
                 const data = response.data as MoviePage;
-                setPageNumber(data.number);
+                setPage(data);
             });
-        },[]);
-        // useEffect so executa a função quando carrega o componente
+    }, [pagenumber]); //nesse momento o useEffect depende do pagenumber
+    //sempre que mudar o pageNumber ele refaz a requisição e atualizar a pagina.
+    // useEffect so executa a função quando carrega o componente
+    //a execução é controlada pela requisição 
 
+    /* const movie = {
+         id: 1,
+         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+         title: "The Witcher",
+         count: 2,
+         score: 4.5
+     };
+     */
+
+     //o atributo key é uma exigencia do React qunando se vai fazer uma renderizção dinamica de coleção
     return (
         <>
-            <p>{pagenumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                )}
                 </div>
             </div>
 
